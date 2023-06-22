46b4088bfae1186d5a3274c6ae7f97642481d24d
Jean Port <pierreanatc522@gmail.com>
2023-06-21 22:07:10 -0300

    DTO

--- backend/src/main/java/com/projetocatalog/course/catalog/resources/CategoryResource.java
+++ backend/src/main/java/com/projetocatalog/course/catalog/resources/CategoryResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.projetocatalog.course.catalog.dto.CategoryDTO;
 import com.projetocatalog.course.catalog.entities.Category;
 import com.projetocatalog.course.catalog.services.CategoryService;
 
@@ -18,8 +19,8 @@ public class CategoryResource {
 	private CategoryService service;
 	
 	@GetMapping
-	public ResponseEntity<List<Category>> findAll() {
-		List<Category> list = service.findAll();
+	public ResponseEntity<List<CategoryDTO>> findAll() {
+		List<CategoryDTO> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 		
 	}
--- backend/src/main/java/com/projetocatalog/course/catalog/services/CategoryService.java
+++ backend/src/main/java/com/projetocatalog/course/catalog/services/CategoryService.java
@@ -1,10 +1,12 @@
 package com.projetocatalog.course.catalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.projetocatalog.course.catalog.dto.CategoryDTO;
 import com.projetocatalog.course.catalog.entities.Category;
 import com.projetocatalog.course.catalog.repositories.CategoryRepository;
 
@@ -14,8 +16,20 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository repository;
 	
-	public List<Category> findAll() {
-		return repository.findAll();
+	public List<CategoryDTO> findAll() {
+		List<Category> list = repository.findAll();
+		
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+		/*
+		 * 
+		 * Forma 2 de fazer!!
+		List<CategoryDTO> listDtos = new ArrayList<>();
+		
+		for (Category category : list) {
+			listDtos.add(new CategoryDTO(category));
+		}
+		return listDtos;
+		*/
+	
 	}
-
 }
